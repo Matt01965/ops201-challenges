8d1486d327079a5f744dcba1d8cc5db7ce5c927d
matt01965 <mcearles@hotmail.com>
2023-10-24 16:21:26 -0400

    first script

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge Class 02
+# Author:                       Matthew Earles
+# Date of latest revision:      10/24/2023
+# Purpose:                      Ops Challenge 02
+
+# Declaration of variables
+my_variable="Hello World!"
+perfection="I am working perfectly.  Thank the Maker."
+# Declaration of functions
+
+
+# Main
+
+echo $my_variable
+echo $perfection
+
+
+# End
