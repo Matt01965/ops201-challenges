f6a84d73b7f61cdbf7d6c91f6f9a2fbd94d18851
Matthew Earles <142598248+Matt01965@users.noreply.github.com>
2023-11-08 12:54:37 -0800

    Create domainanalyzer.sh

---
+++ domainanalyzer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script Name:                  Code Challenge 13
+# Author:                       Matthew Earles
+# Date of latest revision:      11/8/2023
+# Purpose:                      Provide Domain information
+
+
+
+# Ask the user to enter a domain name
+echo "Enter a domain name"
+read domain
+
+ProvidesDomaininformation (){
+    # Display whois information about the domain
+    echo "WHOIS information:" >> test.txt
+    whois $domain >> test.txt
+
+    # Display DNS information about the domain using dig
+    echo "DNS information (dig):" >> test.txt
+    dig $domain >> test.txt
+
+    # Display DNS information about the domain using host
+    echo "DNS information (host):" >> test.txt
+    host $domain >> test.txt
+
+    # Display DNS information about the domain using nslookup
+    echo "DNS information (nslookup):" >> test.txt
+    nslookup $domain >> test.txt
+}
+ProvidesDomaininformation
